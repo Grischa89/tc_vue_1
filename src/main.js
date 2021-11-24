fbc3f8d1c5a4f5290ec2816bcbb68f6098ed79c6
Jana Wernick <jana.wernick@gmail.com>
2021-11-24 16:32:44 +0100

    Added and configured tailwindcss

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import './tailwind.css'
 
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/'
 axios.defaults.baseURL = 'http://api.trainercodes.tk/'
