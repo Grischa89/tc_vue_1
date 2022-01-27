740c596672239cbe9de334e8354614570cc53937
Jana Wernick <jana.wernick@gmail.com>
2022-01-27 12:48:59 +0100

    Minor changes in Table component:
    Excluded title to make it a component in the future
    MAde table style moe neutral

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="flex flex-col justify-around items-center">
-    Hi, I'm going to be a table component!
+
+    <h1 v-if="title" class="text-lg font-bold uppercase tracking-wider my-2">{{ title }}</h1>
+    <h1 v-else class="text-lg font-bold uppercase tracking-wider my-2">Default title</h1>
 
     <!-- For small devices table has width of 11/12, from sm-breakpoint on has max-width of md (448px) -->
-    <div class="flex flex-col mx-auto my-4 bg-white rounded-lg shadow border-b border-gray-300 w-10/12 sm:max-w-md">
+    <div class="flex flex-col mx-auto my-2 py-1 bg-white rounded shadow border-b border-gray-300 w-10/12 sm:max-w-md">
       <!-- Alternative width proportions: w-10/12 sm:w-8/12 md:w-6/12 lg:max-w-md -->
-      <h1 v-if="title" class="text-lg font-bold uppercase tracking-wider my-4">{{ title }}</h1>
-      <h1 v-else class="text-lg font-bold uppercase tracking-wider my-4">Default title</h1>
+      
       <table class="table-fixed text-sm">
-        <thead>
+        <thead class="border-b border-gray-300">
           <tr class="thead-row">
             <th scope="col" class="w-2/5 py-2 px-4 text-left uppercase">Country</th>
             <th scope="col" class="w-2/5 py-2 px-4 text-left uppercase">Trainercode</th>
@@ -17,7 +18,7 @@
         </thead>
         <tbody class="divide-y divide-gray-300">
           <tr v-for="(code, i) in codes" :key="i" class="">
-            <td class="py-4 px-4 text-left">
+            <td class="py-4 px-4 text-left font-bold">
                 <router-link :to="`/${code.continent_slug}/${code.country_slug}`">{{ code.country }}</router-link>
                 <!-- <router-link :to="{ name: country, params: { continent: code.continent_slug, country: country_slug } }">{{ code.country }}</router-link> -->
             </td>
@@ -69,9 +70,11 @@ export default {
 
 <style scoped>
 
-  tr:nth-of-type(odd) {
-    background: rgb(209, 213, 219);
-  }
+  /* tr:nth-of-type(odd) {
+    //background: rgb(209, 213, 219);
+    //background: rgb(255, 227, 192);
+    // background: #fed7aa;
+  } */
   thead .thead-row {
     background: white;
   }
