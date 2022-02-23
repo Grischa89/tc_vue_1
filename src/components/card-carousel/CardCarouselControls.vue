835a6691fc7c3094560ec1e4d3898fca154aa142
Jana Wernick <jana.wernick@gmail.com>
2022-02-23 22:39:41 +0100

    Changed svg style of carousel controls

--- src/components/card-carousel/CardCarouselControls.vue
+++ src/components/card-carousel/CardCarouselControls.vue
@@ -2,13 +2,13 @@
   <div class="carousel__control">
     <button @click="$emit('prev', $event)"
       class="carousel__control__button">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
       </svg>
     </button>
     <button @click="$emit('next', $event)"
       class="carousel__control__button">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
       </svg>
     </button>
@@ -31,5 +31,10 @@ export default {
   height: 50px;
   color: #000;
   cursor: pointer;
+
+  & svg {
+    height: 1.5rem;
+    width: 1.5rem;
+  }
 }
 </style>
