9864bf38aa9d6a13fc8c2a69d9654eca8ecea834
Jana Wernick <jana.wernick@gmail.com>
2022-02-25 18:27:06 +0100

    Added copy button + logic to CardCarouselSlide and adjusted styling

--- src/components/card-carousel/CardCarouselSlide.vue
+++ src/components/card-carousel/CardCarouselSlide.vue
@@ -2,12 +2,16 @@
   <!-- data-pos crucial for sliding to work (getting and re-calculating item position) -->
   <li
     :data-pos="code.dataPos"
+    :data-code="code.player_code"
   >
-    <p class="carousel__item__heading">{{ code.country }}</p>
+    <div class="carousel__item__heading">{{ code.country }}</div>
 
     <qrcode-vue :value="code.player_code" level="H"/>
 
-    <p class="carousel__item__code">{{ code.player_code }}</p>
+    <div class="carousel__item__code">{{ code.player_code }}</div>
+
+    <CopyButton 
+      @click="copyCodeToClipboard($event)" />
 
     <!-- TODO: Remove for productiion: -->
     <p id="carousel__item__index">{{ index }}</p>
@@ -18,12 +22,14 @@
 
 <script>
 import QrcodeVue from 'qrcode.vue';
+import CopyButton from '../copy-button/CopyButton.vue';
 
 export default {
   name: 'CardCarouselSlide',
 
   components: {
       QrcodeVue,
+      CopyButton,
   },
 
   props: {
@@ -40,17 +46,23 @@ export default {
     },
   },
 
+  methods: {
+    copyCodeToClipboard(e) {
+      const currentSlide = e.target.closest('.carousel__item');
+      this.$store.dispatch('copyCodeToClipboard', currentSlide.dataset.code);
+    }
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
 .carousel__item {
-  & p {
+  & div {
     font-size: .875rem;
     word-wrap: break-word;
     line-height: 1.25rem;
     margin-top: .75rem;
-    margin-bottom: 1.25rem;
     text-transform: uppercase;
     max-width: 80%;
   }
@@ -58,7 +70,25 @@ export default {
   &__heading {
     font-weight: 700;
     letter-spacing: 0.05em;
+    margin-bottom: .75rem;
   }
+
+  &__code {
+    margin-bottom: .25rem;
+  }
+
+  .button {
+    margin-bottom: .75rem;
+  }
+
+  // TODO:
+  // Optional styling to hide copy buttons on all but [data-pos="0"]
+  // &:not([data-pos="0"]) {
+
+  //   .button {
+  //     display: none;
+  //   }
+  // }
 }
 
 // TODO: Remove before production:
