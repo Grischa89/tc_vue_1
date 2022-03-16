057851e3b99a30d526bd9ed1ca1fd1af92caa65c
Jana Wernick <jana.wernick@gmail.com>
2022-03-16 09:28:41 +0100

    feature: Add validation for all input fields
    Added validation per field @blur and also @click in postCode method.
    Added --error classes to input and ptentially label.

--- src/views/AddCode.vue
+++ src/views/AddCode.vue
@@ -5,16 +5,17 @@
       <h2 class="form__title">Add Your Own Code</h2>
 
       <div class="form__group">
-        <label class="form__group__label" for="trainercode">Trainercode</label>
-        <input class="form__group__input form__group__input--code" type="number" step="1" id="trainercode" name="trainercode" placeholder="0000 1111 2222" v-model="data.player_code" @keydown="restrictKeys($event)" @keyup="typeCode($event)" @paste="pasteCode($event)" required>
+        <label class="form__group__label" :class="{'form__group__label--error': errors.invalidCode}" for="trainercode">Trainercode</label>
+        <input class="form__group__input form__group__input--code" :class="{'form__group__input--error': errors.invalidCode}" type="number" step="1" id="trainercode" name="trainercode" placeholder="0000 1111 2222" v-model="data.player_code" @keydown="restrictKeys($event)" @keyup="typeCode($event)" @paste="pasteCode($event)" @blur="validateCode(data.player_code)" required>
 
-        <!-- <span v-if="error" class="form__group__help">Please enter a valid trainercode.</span> -->
+        
         <span v-if="formatCode" class="form__group__help">Your Trainer Code: {{ formatCode }}</span>
+        <span v-if="errors.invalidCode" class="form__group__help" :class="{'form__group__help--error': errors.invalidCode}">{{ errors.invalidCode }}</span>
       </div>
 
       <div class="form__group">
         <label class="form__group__label" for="country">Country</label>
-        <select class="form__group__input form__group__input--select" name="country" id="country" @change="getCountry($event)" v-model="data.country">
+        <select class="form__group__input form__group__input--select" name="country" id="country"  @change="getCountry($event)" @blur="validateCountry(data.country)" v-model="data.country">
           <option value selected disabled >Choose A Country</option>
           <option
             v-for="(country, i) in countries"
@@ -22,18 +23,14 @@
             :value="country.slug">{{ country.name }}</option>
         </select>
         
-        <!-- <span class="form__group__help"></span> -->
+        <span v-if="errors.invalidCountry" class="form__group__help" :class="{'form__group__help--error': errors.invalidCountry}">{{ errors.invalidCountry }}</span>
       </div>
 
       <div class="form__group">
         <label class="form__group__label" for="city">City</label>
         <!-- disabled until country got chosen successfully -->
-        <select class="form__group__input form__group__input--select" name="city" id="city" v-model="data.city" :disabled="!cities">
+        <select class="form__group__input form__group__input--select" name="city" id="city" @change="validateCity(data.city)" @blur="validateCity(data.city)" v-model="data.city" :disabled="!cities">
           <option value selected disabled>Choose A City</option>
-          <!-- <option value="berlin">Berlin</option>
-          <option value="bremen">Bremen</option>
-          <option value="hamburg">Hamburg</option>
-           -->
           <option 
             v-for="(city, i) in cities"
             :key="i" 
@@ -41,7 +38,7 @@
           </option>
         </select>
 
-        <!-- <span class="form__group__help"></span> -->
+        <span v-if="errors.invalidCity" class="form__group__help" :class="{'form__group__help--error': errors.invalidCity}" >{{ errors.invalidCity }}</span>
       </div>
 
       <button @click="postCode">Save</button>
@@ -50,25 +47,6 @@
 
     
   </div>
-
-  <div>
-    {{ data.player_code.length }}
-  </div>
-
-  <!-- <div v-if="cities">
-      cities:
-      {{ cities }}
-    </div>
-
-    <div>
-      data:
-      {{ data }}
-    </div>
-
-    <div>
-      codeToAdd:
-      {{ codeToAdd }}
-    </div> -->
 </template>
 
 <script>
@@ -83,6 +61,11 @@ export default {
         country: '',
         city: '',
         player_code: '',
+      },
+      errors: {
+        invalidCode: '',
+        invalidCountry: '',
+        invalidCity: ''
       },
       codeInput: '',
       // inputLen: '',
@@ -101,10 +84,6 @@ export default {
       cities: 'cities',
     }),
 
-    // cityList() {
-    //   return this.cities;
-    // },
-
     codeToAdd() {
       return this.data;
     },
@@ -117,10 +96,19 @@ export default {
 
   methods: {
     postCode() {
-      this.$store.dispatch('addCode', this.data)
-        .then(() => {
-          this.$router.push(`/`);
-        });
+      const validCode = this.validateCode(this.data.player_code);
+      const validCountry = this.validateCountry(this.data.country);
+      const validCity = this.validateCity(this.data.city);
+
+      if (!validCode || !validCountry || !validCity) return false;
+      
+      if (validCode && this.data.country && this.data.city) {
+        console.log('data', this.data);
+      } 
+      // this.$store.dispatch('addCode', this.data)
+      //   .then(() => {
+      //     this.$router.push(`/`);
+      //   });
 
       // TODO: Catch error!
     },
@@ -128,12 +116,9 @@ export default {
     getCountry(event) {
       console.log('event.target.value', event.target.value);
 
+      this.validateCountry(event.target.value);
       this.$store.dispatch('getCities', event.target.value);
     },
-
-    // Dies sollte in length condition geprüft werden = dann, wenn Code EIngabe maximale
-    // Zeichenzahl erreicht hat
-    // (df.player_code.str.count("0001|0000|1111|2222|3333|4444|5555|6666|7777|8888|9999|1234|4321|5678|8765|6789|9876") < 3)
 
     // This can also prevent copy + paste invalid character
     // filterInput(e){
@@ -141,6 +126,7 @@ export default {
     // }
 
     restrictKeys(e) {
+      this.errors.invalidCode = '';
       // Must happen @keydown to preventing from being typed
       // TODO e.key and e.code not detected on mobile
       // if ready code contains any other char than num find index of that char and remove it
@@ -180,6 +166,46 @@ export default {
       this.data.player_code = input;
     },
 
+    validateCode(code) {
+      if (!code) {
+        this.errors.invalidCode = 'Please enter a Trainer Code.';
+        return false;
+      }
+
+      let patternCount = (code.match(/000|0000|0001|1111|2222|3333|4444|5555|6666|7777|8888|9999|1234|4321|5678|8765|6789|9876/g) || []).length;
+
+      if (patternCount > 2) {
+        this.codeInput = '';
+        this.errors.invalidCode = 'Please enter a valid Trainer Code.';
+        return false;
+      } else {
+        this.errors.invalidCode = '';
+        return true;
+      }
+    },
+
+    validateCountry(country) {
+      if (!country) {
+        this.errors.invalidCountry = 'Please choose a country.';
+        console.log('this.errors.invalidCountry', this.errors.invalidCountry);
+        return false;
+      } else {
+        this.errors.invalidCountry = '';
+        return true;
+      }
+    },
+
+    validateCity(city) {
+      if (!city) {
+        this.errors.invalidCity = 'Please choose a city.';
+        console.log('this.errors.invalidCity', this.errors.invalidCity);
+        return false;
+      } else {
+        this.errors.invalidCity = '';
+        return true;
+      }
+    }
+
   },
 
 }
@@ -193,6 +219,7 @@ export default {
 
 .form {
   &__group {
+
     display: flex;
     flex-direction: column;
     align-items: flex-start;
@@ -204,6 +231,10 @@ export default {
       letter-spacing: .05rem;
       font-weight: bold;
       margin-bottom: .25rem;
+
+      // &--error {
+      //   color: rgb(200, 0, 0);
+      // }
     }
 
     &__input {
@@ -226,6 +257,12 @@ export default {
         margin: 0;
       }
 
+
+
+      &--error {
+        border-color: rgb(200, 0, 0);
+      }
+
       // &--select {
       //   // padding-right: 1rem;
       // }
@@ -235,6 +272,10 @@ export default {
       font-size: .75rem;
       margin-top: .25rem;
       // color: rgb(200, 0, 0);
+
+      &--error {
+        color: rgb(200, 0, 0);
+      } 
     }
   }
 }
