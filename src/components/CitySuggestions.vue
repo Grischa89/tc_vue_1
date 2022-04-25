d320652425bc49a2586805965b359ee342c1233f
Jana Wernick <jana.wernick@gmail.com>
2022-04-25 14:34:57 +0200

    style/refactor: Change hierarchy of css

--- src/components/CitySuggestions.vue
+++ src/components/CitySuggestions.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="suggestions">
-    <!-- {{ slicedSuggestions }} -->
-    <div
-      v-for="(city, i) in slicedSuggestions"
-      :key="i"
-      class="suggestions__item">
-        <router-link class="suggestions__item__link" :to="`/${city.continent_slug}/${city.country_slug}/${city.city_slug}`">{{ city.city }}</router-link>
+    <!-- <h2 class="suggestions__title">Popular Cities</h2> -->
+    <div class="suggestions__grid">
+      <div
+        v-for="(city, i) in slicedSuggestions"
+        :key="i"
+        class="suggestions__grid__item">
+          <router-link class="suggestions__grid__item__link" :to="`/${city.continent_slug}/${city.country_slug}/${city.city_slug}`">{{ city.city }}</router-link>
+      </div>
     </div>
     <!-- <div class="suggestions__item">City</div>
     <div class="suggestions__item">City</div>
@@ -24,13 +26,17 @@ export default {
   props: {
     citySuggestions: {
       type: Array,
-      // required: true,
+      required: true,
     }
   },
 
   computed: {
     slicedSuggestions() {
-      if (this.citySuggestions) return this.citySuggestions.slice(0, 6);
+      if (this.citySuggestions) {
+        // Sort citySuggestions array in descending order by city_ranking
+        const sortByCityRanking = this.citySuggestions.sort((a, b) => b.city_ranking - a.city_ranking);
+        return sortByCityRanking.slice(0, 6);
+      }
     }
   }
 
@@ -38,20 +44,29 @@ export default {
 </script>
 
 <style lang="scss">
-// $color-picker: $primary, $primary-darker, $in-between, $secondary-lighter;
-
 .suggestions {
-  display: grid;
-  gap: .5rem;
-  height: 100%;
-  // width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 90%;
   margin: 1rem auto;
-  padding: 2rem 1rem;
-  grid-template-columns: repeat(6, 1fr);
-  background: hsla(0, 0%, 100%, 1);
-  // background: linear-gradient($primary-gradient);
-  // background: -webkit-linear-gradient($primary-gradient);
-  // background: -moz-linear-gradient($primary-gradient);
+
+  // &__title {
+
+  // }
+
+  &__grid {
+    display: grid;
+    gap: .5rem;
+    height: 100%;
+    margin: .5rem auto;
+    // padding: 2rem 1rem;
+    grid-template-columns: repeat(6, 1fr);
+    background: hsla(0, 0%, 100%, 1);
+    // background: linear-gradient($primary-gradient);
+    // background: -webkit-linear-gradient($primary-gradient);
+    // background: -moz-linear-gradient($primary-gradient);
 
   &__item {
     display: flex;
@@ -63,6 +78,8 @@ export default {
     // border-radius: .75rem;
     grid-column: span 2;
     min-height: 3rem;
+    word-break: break-all;
+    word-wrap: break-word;
 
     // background: hsla(33, 91%, 62%, 1);
     // background: linear-gradient(90deg, hsla(33, 91%, 62%, 1) 10%, hsla(33, 91%, 50%, 1) 80%);
@@ -79,8 +96,7 @@ export default {
     font-weight: 600;
     font-size: $mobile-body;
     // text-transform: uppercase;
-    word-break: break-all;
-    word-wrap: break-word;
+    
 
     &__link {
       display: -webkit-box;
@@ -92,10 +108,6 @@ export default {
         text-decoration: underline;
       }
     }
-
-
-
-
     
     // box-shadow: $card-shadow rgba($black, 0.2); 
     // -webkit-box-shadow: $card-shadow rgba($black, 0.2); 
@@ -107,5 +119,6 @@ export default {
     //   }
     // }
   }
+}
 }
 </style>
