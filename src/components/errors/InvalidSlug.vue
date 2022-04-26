47f116ae78a5d298fcc5e73e8fd5b6f4aa83a0ed
Jana Wernick <jana.wernick@gmail.com>
2022-04-26 12:32:34 +0200

    style: Restructure css + bem class names for user info box

--- src/components/errors/InvalidSlug.vue
+++ src/components/errors/InvalidSlug.vue
@@ -1,18 +1,22 @@
 <template>
-  <div v-if="!showInfo" class="user__info__show">
-    <svg @click="showInfo = !showInfo" xmlns="http://www.w3.org/2000/svg" class="user__info__show__btn" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
-  </div>
-  <div v-if="showInfo" class="user__info user__info--info">
-    <h3 class="user__info__title">Invalid URL</h3>
-    <div class="user__info__dismiss" @click="showInfo = !showInfo">
-      <svg xmlns="http://www.w3.org/2000/svg" class="user__info__dismiss__btn" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <div class="user__info">
+    <div v-if="!showInfo" class="user__info__btn user__info__btn--info">
+      <svg @click="showInfo = true" xmlns="http://www.w3.org/2000/svg" class="user__info__show__btn" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-labelledby="show-user-info-box">
+        <title id="show-user-info-box">Show User Info Box</title>
+        <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+    </div>
+    <div v-else class="user__info__btn user__info__btn--info">
+      <svg @click="showInfo = false" xmlns="http://www.w3.org/2000/svg" class="user__info__dismiss__btn" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-labelledby="dismiss-user-info-box">
+      <title id="dismiss-user-info-box">Dismiss User Info Box</title>
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
       </svg>
     </div>
 
-    <p>{{ message }}</p>
+    <div v-if="showInfo" class="user__info__container user__info__container--info">
+      <h3 class="user__info__container__title">Invalid URL</h3>
+      <p class="user__info__container__text">{{ message }}</p>
+    </div>
   </div>
 </template>
 
@@ -40,34 +44,47 @@ export default {
   position: relative;
   font-size: $mobile-body;
   font-weight: normal;
-  text-align: left;
-  // max-width: 24.3076923rem;
   width: 90%;
+  min-height: 1rem;
   margin: 1rem auto 1.5rem auto;
-  padding: .5rem;
-  border-radius: .25rem;
-  background-color: rgba($help, .1);
-  box-shadow: 0 0 0 2px $help-shadow;
-  color: rgba($black, .7);
+
+  
+
+  &__btn {
+    position: absolute;
+    top: .375rem;
+    right: .375rem;
+    margin-left: 2rem;
+    height: $mobile-heading;
+    width: $mobile-heading;
+    cursor: pointer;
+
+    &--info {
+      color: $info
+    }
+
+    &--error {
+      color: $error
+    }
+
+    &--success {
+      color: $success
+    }
+  }
+
+  &__container {
+    width: 100%;
+    text-align: left;
+    background-color: rgba($help, .1);
+    box-shadow: 0 0 0 2px $help-shadow;
+    color: rgba($black, .7);
+    border-radius: .25rem;
+    padding: .5rem;
 
     &__title {
       margin-bottom: .25rem;
       font-size: $mobile-subheading;
       font-weight: bold;
-    }
-
-    &__dismiss {
-      position: absolute;
-      top: .375rem;
-      right: .375rem;
-      // display: flex;
-      // justify-content: flex-end;
-      margin-left: 2rem;
-
-      &__btn {
-        height: $mobile-subheading;
-        width: $mobile-subheading;
-      }
     }
 
     &--info {
@@ -87,30 +104,14 @@ export default {
       box-shadow: 0 0 0 2px $success-shadow;
       color: $success
     }
+  }
 
-    &--highlight {
-        font-weight: bold;
-    }
+  // &--highlight {
+  //     font-weight: bold;
+  // }
 
-    &__link {
-        font-weight: bold;
-    }
-}
-
-.user__info__show {
-  position: relative;
-  font-weight: normal;
-  width: 90%;
-  margin: 1rem auto 1.5rem auto;
-  padding: .5rem;
-  border-radius: .25rem;
-
-  &__btn {
-    height: $mobile-subheading;
-    width: $mobile-subheading;
-    position: absolute;
-      top: .375rem;
-      right: .375rem;
+  &__link {
+      font-weight: bold;
   }
 }
 </style>
