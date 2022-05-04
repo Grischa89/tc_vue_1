031d53c2b492ab067fb16c5f6e275194c9b88f9e
Jana Wernick <jana.wernick@gmail.com>
2022-05-04 12:48:19 +0200

    refactor: Use slots in async error component to make it make dynamic and reusable

---
+++ src/components/errors/ErrorFetchingData.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="error__page">
+
+    <h1 class="error__page__title">
+      <slot name="title"></slot>
+    </h1>
+    <!-- Sorry &#128533; -->
+
+    <h2 class="error__page__text">
+      <slot name="text">{{ defaultMessage }}</slot>
+    </h2>
+
+    <div class="error__page__btn__container">
+      
+      <slot name="action">
+        <!-- <router-link :to="{ name: 'Home' }">Back to Home</router-link> -->
+      </slot>
+    
+      <button class="error__page__btn error__page__btn--refresh" @click="refreshPage">
+        Try Again
+      </button>    
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorFetchingData',
+
+  data() {
+    return {
+      defaultMessage: 'Something went wrong retrieving data.',
+    }
+  },
+
+  methods: {
+    refreshPage() {
+      this.$router.go();
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+.error__page {
+  background-color: #fff;
+  border-radius: 0.75em;
+  width: 90%;
+  padding: 2rem;
+  margin: auto;
+
+  @media(min-width: 1024px) {
+    width: 50%;
+    max-width: 24.3076923rem;
+  }
+
+  &__title {
+    // TODO: Better name
+    font-size: 2rem;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  &__text {
+    margin: 1.5rem auto;
+    // TODO: heading
+    font-size: $mobile-heading;
+    // font-weight: bold;
+    // text-transform: uppercase;
+    width: 80%;
+  }
+
+  &__btn__container {
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__btn {
+    margin: .5rem auto;
+    padding: .75em 2.5em;
+    text-transform: uppercase;
+    font-size: $mobile-body;
+    letter-spacing: .05rem;
+    font-weight: bold;
+    border-radius: 25px;
+    -webkit-tap-highlight-color: transparent;
+
+    &:active {
+      // outline: none;
+      // box-shadow: 0 0 0 2px $help-shadow;
+      -webkit-tap-highlight-color: transparent;
+    }
+
+    &--action {
+      color: $white;
+      background-color: $black;
+    }
+
+    &--reload {
+      color: $black;
+      background: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ import './tailwind.css'
 import VueCookieComply from 'vue-cookie-comply'
 import 'vue-cookie-comply/dist/style.css'
 
-const AsyncCodesNotFound = defineAsyncComponent(() => import('./components/errors/CodesNotFound.vue'));
+const AsyncErrorFetchingData = defineAsyncComponent(() => import('./components/errors/ErrorFetchingData.vue'));
 const AsyncInvalidSlug = defineAsyncComponent(() => import('./components/errors/InvalidSlug.vue'));
 
 axios.defaults.baseURL = 'https://api.trainercodes.tk/'
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/'
-createApp(App).use(store).use(router, axios).use(VueCookieComply).component('AsyncCodesNotFound', AsyncCodesNotFound).component('AsyncInvalidSlug', AsyncInvalidSlug).mount('#app')
+createApp(App).use(store).use(router, axios).use(VueCookieComply).component('AsyncErrorFetchingData', AsyncErrorFetchingData).component('AsyncInvalidSlug', AsyncInvalidSlug).mount('#app')
