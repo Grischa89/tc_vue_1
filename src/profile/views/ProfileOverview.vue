1c00bf314391c330bdabe595879fec4146abb452
Jana Wernick <jana.wernick@gmail.com>
2022-05-26 14:43:21 +0200

    feature: Add link to add a subscription

--- src/profile/views/ProfileOverview.vue
+++ src/profile/views/ProfileOverview.vue
@@ -35,7 +35,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
           </svg>
         </router-link>
-        <router-link class="profile__overview__link" to="#">
+        <router-link class="profile__overview__link" :to="{ name: 'AddSubscription'}">
           <span class="profile__overview__link__text">Add subscription</span>
           <svg xmlns="http://www.w3.org/2000/svg" class="profile__overview__link__append" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-labelledby="add-subscription">
               <title id="add-subscription">Add subscription</title>
