3903e3062b51cfaa45eb104088673ac2567f5d40
Jana Wernick <jana.wernick@gmail.com>
2022-07-08 10:18:37 +0200

    feature: Add basic articles components + logic

---
+++ src/views/articles/ListArticles.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-if="articles" class="articles">
+        List article
+        <ArticlePreview 
+            v-for="(article, i) in articles"
+            :key="i"
+            :article="article" />
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import ArticlePreview from './components/ArticlePreview.vue'
+
+export default {
+    name: 'ListArticles',
+
+    components: {
+        ArticlePreview,
+    },
+
+    created() {
+        this.$store.dispatch('getArticles');
+    },
+
+    computed: {
+        ...mapGetters({
+            articles: 'articles',
+        })
+    }
+
+}
+</script>
+<style lang="scss">
+    .articles {
+        width: 95%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
---
+++ src/views/articles/ShowArticle.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <ArticleDisplay
+        :article="article" />
+    </div>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+
+import ArticleDisplay from '../articles/components/ArticleDisplay.vue';
+
+export default {
+    name: 'ShowArticle',
+
+    components: {
+        ArticleDisplay,
+    },
+
+    created() {
+        this.$store.dispatch('getArticle', this.$route.params.slug);
+    },
+
+    computed: {
+        ...mapGetters({
+            article: 'displayArticle',
+        })
+    }
+}
+</script>
+<style lang="scss">
+    .article {
+
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: $mobile-body;
+        text-align: justify;
+
+        &__heading {
+            font-family: Monaco, monospace;
+            font-size: 1.75rem;
+            font-weight: bold;
+            letter-spacing: .0875rem;
+            margin-bottom: 1rem;
+        }
+
+        &__summary {
+            color: var(--help);
+            text-align: justify;
+            padding-left: 1rem;
+            padding-right: 1rem;
+            margin-bottom: 1rem;
+        }
+
+        &__meta {
+            color: var(--help);
+
+            span {
+                margin-right: 1rem;
+            }
+        }
+
+        &__subheading {
+            font-family: Monaco, monospace;
+            font-size: 1.5rem;
+            font-weight: 600;
+            font-style: italic;
+            margin-top: 1rem;
+            margin-bottom: 1rem;
+        }
+
+        &__sub-subheading {
+            font-family: Monaco, monospace;
+            font-size: 1.25rem;
+            padding-left: 1rem;
+            margin-top: 1rem;
+            margin-bottom: .5rem;
+        }
+
+        &__paragraph {
+            text-indent: 1rem;
+        }
+
+        &__listarray {
+            list-style-type: '—';
+            list-style-position: outside;
+            text-align: left;
+            margin-top: 1rem;
+            margin-bottom: 1rem;
+            &__item { 
+                margin-top: .5rem;
+                margin-left: 1.75rem;
+                padding-left: .5rem;
+            }
+        }
+    }
+</style>
