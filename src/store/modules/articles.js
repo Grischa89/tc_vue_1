9ab7cf73ee246caa5268b96a320f3d9761c3dd20
Jana Wernick <jana.wernick@gmail.com>
2022-07-21 11:38:06 +0200

    fix: Split listarray along semicolon to be able to use commas inside list items

--- src/store/modules/articles.js
+++ src/store/modules/articles.js
@@ -28,27 +28,70 @@ const getters = {
     },
 
     displayArticle: state => {
-        // const order = state.article?.order;
-        // const articleTexts = state.article.filter(item => )
+        if (state.article) {
+            const article = state.article;
+            const order = state.article.order;
+            delete article.slug;
+            delete article.order;
 
-        // const content = state.article;
-        // const order = content.order;
-        // delete content.slug;
-        // delete content.order;
+            // Sort out sections that have null value
+            for (const section in article) {
+                if ((section === 'table' && article[section].shape_1 === null) || article[section] === null) delete article[section];
+            }
+            // Necessary to keep track of possible tables (1-3)
+            let tableCounter = 0;
 
-        // for (const key in content) {
-        //     if ((key === 'table' && content[key].headers_1 === null) || content[key] === null) delete content[key];
-        // }
+            // According to the order of elements of order array create new loopable array
+            const articleOrdered = order.map(element => {
+                // Object to be returned from map
+                const articleRow = {
+                    section: '',
+                    content: {}
+                };
 
-        // let keys = Object.keys(content);
-        // = content.map(element => {
-        //     // Object.keys() returns an array: ['heading']
-        //     return Object.keys(element);
-        // });
+                // Section value is of type array (subheading, sub_subheading, paragraph, listarray)
+                if (Array.isArray(article[`${element}`])) {
+                    articleRow.section = element;
+                    articleRow.content = element === 'listarray' ? article[`${element}`][0].split(';') : article[`${element}`][0];
+                    article[`${element}`].shift();
+                    return articleRow;
+                }
 
+                // Section value is of type object (table)
+                if (!Array.isArray(article[`${element}`]) && typeof article[`${element}`] === 'object') {
+                    tableCounter++;
 
-        // console.log('%ctype', 'color: darkseagreen; font-weight: bold;', keys, order);
-        return state.article;
+                    // Make sure table exists
+                    if (article[`${element}`][`shape_${tableCounter}`].length > 0) {
+                        const numberOfColumns = parseInt
+                        (article[`${element}`][`shape_${tableCounter}`].split(',')[1]);
+
+                        const columns = article[`${element}`][`table_head_${tableCounter}`];
+                        // Array that will contain rows (as arrays)
+                        const rows = [];
+
+                        // Increment in steps of # of columns
+                        for (let i = 0; i < article[`${element}`][`table_text_${tableCounter}`].length; i+= numberOfColumns) {
+                            const row = article[`${element}`][`table_text_${tableCounter}`].slice(i, i + numberOfColumns);
+                            rows.push(row);
+                        }
+
+                        articleRow.section = element;
+                        articleRow.content['columns'] = columns;
+                        articleRow.content['rows'] = rows;
+                        return articleRow;
+                    }
+                }
+
+                // Section that only occurs once (heading, summary)
+                articleRow.section = element;
+                articleRow.content = article[`${element}`];
+
+                return articleRow;
+            });
+
+            return articleOrdered;
+        }
     }
 };
 
@@ -89,8 +132,9 @@ const actions = {
 
         axios.get(`/api/v1/articles/${slug}`)
             .then(res => {
+                const art = res.data;
                 console.log('%cres', 'color: darkseagreen; font-weight: bold;', res.data);
-                commit('setArticle', res.data);
+                commit('setArticle', art);
             })
             .catch(err => {
                 console.log('%cerr', 'color: red; font-weight: bold;', err);
