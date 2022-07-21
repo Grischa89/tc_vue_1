12cc4aa56b360cf1652e91f548c3b5ee4f9c1e91
Jana Wernick <jana.wernick@gmail.com>
2022-07-21 12:12:59 +0200

    style: Set styles for table in article

--- src/views/articles/components/ArticleTemplate.vue
+++ src/views/articles/components/ArticleTemplate.vue
@@ -13,13 +13,7 @@ export default {
 
         font-family: Arial, Helvetica, sans-serif;
         font-size: $mobile-body;
-        text-align: left;
-
-        @include tablet {
-            width: 60ch;
-            margin-left: auto;
-            margin-right: auto;
-        }
+        text-align: justify;
 
         &__heading {
             font-family: Monaco, monospace;
@@ -32,13 +26,12 @@ export default {
         &__summary {
             color: var(--help);
             text-align: justify;
-            padding-left: 2rem;
-            padding-right: 2rem;
+            padding-left: 1rem;
+            padding-right: 1rem;
             margin-bottom: 1rem;
         }
 
         &__meta {
-            font-size: $mobile-help;
             color: var(--help);
 
             span {
@@ -50,7 +43,6 @@ export default {
             font-family: Monaco, monospace;
             font-size: 1.5rem;
             font-weight: 600;
-            font-style: italic;
             margin-top: 1rem;
             margin-bottom: 1rem;
         }
@@ -58,6 +50,7 @@ export default {
         &__sub-subheading {
             font-family: Monaco, monospace;
             font-size: 1.25rem;
+            font-style: italic;
             padding-left: 1rem;
             margin-top: 1rem;
             margin-bottom: .5rem;
@@ -73,10 +66,97 @@ export default {
             text-align: left;
             margin-top: 1rem;
             margin-bottom: 1rem;
-            &__item {
+            &__item { 
                 margin-top: .5rem;
                 margin-left: 1.75rem;
                 padding-left: .5rem;
+            }
+        }
+
+        &__table {
+            display: flex;
+            flex-direction: column;
+            margin: .5rem auto;
+            border-radius: .75rem;
+            border: .0625rem solid var(--border);
+            width: 100%;
+            max-width: 60ch;
+            background-color: var(--surface2);
+
+            @include tablet {
+                box-shadow: 1px 1px 5px rgba($black, 0.2);
+            }
+
+            &__table {
+                table-layout: fixed;
+                font-size: $mobile-body;
+                width: 100%;
+
+                &__head {
+                    border-bottom: .03125rem solid var(--divider);
+
+                    &__row {
+                        font-family: Monaco, monospace;
+                        font-weight: bold;
+                        letter-spacing: .075ch;
+                        background-color: var(--surface4);
+
+                        &__cell {
+                            padding:1rem;
+                            text-align: left;
+                            text-transform: uppercase;
+                            width: 50%;
+
+                            &:first-of-type {
+                            border-top-left-radius: .75rem;
+                            }
+
+                            &:last-of-type {
+                            border-top-right-radius: .75rem;
+                            }
+                        }
+                    }
+                }
+
+                &__body {
+
+                    &__row {
+                        border-bottom: .0625rem solid var(--divider);
+
+                        &:hover {
+                            background-color: var(--hover-surface);
+                        }
+
+                        &:last-of-type {
+                            border: none;
+                        }
+
+                        &__cell {
+                            text-align: left;
+                            // white-space: nowrap;
+                            padding-top: 1rem;
+                            padding-bottom: 1rem;
+                            padding-left: .75rem;
+                            padding-right: 0;
+                            cursor: pointer;
+                        }
+                    }
+
+                    
+                }
+
+                // table,
+                // td,
+                // th {
+                //     border-collapse: collapse;
+                // }
+
+            //     th,
+            //     td {
+            //         padding: 10px;
+            //         border: solid 1px;
+            //         text-align: center;
+            //     }
             }
         }
     }
