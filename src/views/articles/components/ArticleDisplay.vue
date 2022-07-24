f7bccb8aa65c233d09f569e23f3b660199e5dc44
Jana Wernick <jana.wernick@gmail.com>
2022-07-24 15:03:56 +0200

    fix: Use changed prop name in component

--- src/views/articles/components/ArticleDisplay.vue
+++ src/views/articles/components/ArticleDisplay.vue
@@ -1,37 +1,37 @@
 <template>
     <div>
-        <div class="article" v-for="(item, i) in article" :key="i">
-            <div class="article__heading" v-if="item.section === 'heading'">
-                <h1>{{ item.content }}</h1>
+        <div class="article" v-for="(section, i) in article" :key="i">
+            <div class="article__heading" v-if="section.name === 'heading'">
+                <h1>{{ section.content }}</h1>
             </div>
-            <div class="article__summary" v-if="item.section === 'summary'">
-                <p>{{ item.content }}</p>
+            <div class="article__summary" v-if="section.name === 'summary'">
+                <p>{{ section.content }}</p>
             </div>
-            <div class="article__paragraph" v-if="item.section === 'paragraph'">
-                <p>{{ item.content }}</p>
+            <div class="article__paragraph" v-if="section.name === 'paragraph'">
+                <p>{{ section.content }}</p>
             </div>
-            <div class="article__subheading" v-if="item.section === 'subheading'">
-                <h2>{{ item.content }}</h2>
+            <div class="article__subheading" v-if="section.name === 'subheading'">
+                <h2>{{ section.content }}</h2>
             </div>
-            <div class="article__sub-subheading" v-if="item.section === 'sub_subheading'">
-                <h3>{{ item.content }}</h3>
+            <div class="article__sub-subheading" v-if="section.name === 'sub_subheading'">
+                <h3>{{ section.content }}</h3>
             </div>
-            <div class="article__listarray" v-if="item.section === 'listarray'">
-                <ul v-for="(listItem, j) in item.content" :key="j">
+            <div class="article__listarray" v-if="section.name === 'listarray'">
+                <ul v-for="(listItem, j) in section.content" :key="j">
                     <li>{{ listItem }}</li>
                 </ul>
             </div>
-            <div class="article__table" v-if="item.section === 'table'">
+            <div class="article__table" v-if="section.name === 'table'">
                 <table class="article__table__table">
                     <thead class="article__table__table__head">
                         <tr class="article__table__table__head__row">
                             <td
-                                v-for="(columnName, k) in item.content.columns" :key="k" class="article__table__table__head__row__cell">{{ columnName }}</td>
+                                v-for="(columnName, k) in section.content.columns" :key="k" class="article__table__table__head__row__cell">{{ columnName }}</td>
                         </tr>
                     </thead>
                     <tbody class="article__table__table__body">
                         <tr
-                            v-for="(row, l) in item.content.rows"
+                            v-for="(row, l) in section.content.rows"
                             :key="l"
                             class="article__table__table__body__row">
                             <td
