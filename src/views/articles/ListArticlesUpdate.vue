a66b54dac7be2014eb95137017f196a0dea0db48
Jana Wernick <jana.wernick@gmail.com>
2022-07-25 08:31:02 +0200

    feature: Add basic routing + components for updating an article

---
+++ src/views/articles/ListArticlesUpdate.vue
@@ -0,0 +1,65 @@
+<template>
+    <div v-if="articles" class="articles">
+        <div class="articles__header">
+            <h1 class="articles__header__title">Choose an article to update</h1>
+        </div>
+        <ArticleListPreview 
+            v-for="(article, i) in articles"
+            :key="i"
+            :article="article"
+            :isUpdateRoute="isUpdateRoute" />
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import ArticleListPreview from './components/ArticleListPreview.vue'
+
+export default {
+    name: 'ListArticlesUpdate',
+
+    components: {
+        ArticleListPreview,
+    },
+
+    created() {
+        this.$store.dispatch('getArticles');
+    },
+
+    data() {
+        return {
+            isUpdateRoute: true,
+        }
+    },
+
+    computed: {
+        ...mapGetters({
+            articles: 'articles',
+        }),
+    }
+}
+</script>
+
+<style lang="scss">
+    .articles {
+        width: 95%;
+        max-width: 70ch;
+        margin-left: auto;
+        margin-right: auto;
+
+        &__header {
+            margin-top: 1.5rem;
+            margin-bottom: 1.5rem;
+
+                &__title {
+                    margin-left: auto;
+                    margin-right: auto;
+                    font-size: $mobile-heading;
+                    font-weight: bold;
+                    text-transform: uppercase;
+                    letter-spacing: .05rem;
+                }
+        }
+    }
+</style>
---
+++ src/views/articles/UpdateArticle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        {{ article }}
+    </div>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    name: 'UpdateArticle',
+
+    created() {
+        this.$store.dispatch('getArticle', this.$route.params.slug);
+    },
+
+    computed: {
+        ...mapGetters({
+            article: 'displayArticle',
+        })
+    }
+
+}
+</script>
+<style lang="">
+    
+</style>
