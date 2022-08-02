63db3c3c18e417dac4b402ebf52451da60ff4189
Jana Wernick <jana.wernick@gmail.com>
2022-08-02 13:28:16 +0200

    fix: Clear article form in beforeUnmount hook to not preserve state

--- src/views/articles/UpdateArticle.vue
+++ src/views/articles/UpdateArticle.vue
@@ -19,6 +19,11 @@ export default {
         this.$store.dispatch('getArticle', this.$route.params.slug);
     },
 
+    beforeUnmount() {
+        // Clear article form by setting article to empty string so chosen article will be shown in fomr and NOT article from the last request
+        this.$store.commit('setArticle', '');
+    },
+
     computed: {
         ...mapGetters({
             article: 'articleContent'
