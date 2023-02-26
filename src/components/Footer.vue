9863847e4bfd6c944777c02cded33df3241bd394
Jana Wernick <jana.wernick@gmail.com>
2023-02-26 08:37:36 +0100

    style: Use inline HTML Code emoji instead of svg

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -32,14 +32,10 @@
 
     <div class="footer__container">
       <div class="footer__container__item">
-        &copy; {{ currentYear }} trainercodes.net by // TODO
+        Trainer Codes {{ currentYear }}
       </div>
       <div class="footer__container__item">
-        <svg xmlns="http://www.w3.org/2000/svg" class="footer__container__item__prepend" viewBox="0 0 20 20" fill="currentColor" aria-labelledby="footer-statement">
-          <title id="footer-statement">Footer statement</title>
-          <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
-        </svg>
-        From trainers for trainers.
+        &#129293; From trainers for trainers.
       </div>
     </div>
   </footer>
