a42cfa9a35fa1880ea3e60dd2b072580ef8a9c83
Jana Wernick <jana.wernick@gmail.com>
2023-02-26 21:13:11 +0100

    fix: Apply css to InvalidSlug component

--- src/components/errors/InvalidSlug.vue
+++ src/components/errors/InvalidSlug.vue
@@ -40,85 +40,85 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .invalid-slug__container {
   width: 100%;
   max-width: 80ch;
   margin-left: auto;
   margin-right: auto;
 }
-// .user__info {
-//   position: relative;
-//   font-size: $mobile-body;
-//   font-weight: normal;
-//   width: 90%;
-//   min-height: 1rem;
-//   margin: 1rem auto 1.5rem auto;
+.user__info {
+  position: relative;
+  font-size: $mobile-body;
+  font-weight: normal;
+  width: 90%;
+  min-height: 1rem;
+  margin: 1rem auto 1.5rem auto;
 
   
 
-//   &__btn {
-//     position: absolute;
-//     top: .375rem;
-//     right: .375rem;
-//     margin-left: 2rem;
-//     height: $mobile-heading;
-//     width: $mobile-heading;
-//     cursor: pointer;
+  &__btn {
+    position: absolute;
+    top: .375rem;
+    right: .375rem;
+    margin-left: 2rem;
+    height: $mobile-heading;
+    width: $mobile-heading;
+    cursor: pointer;
 
-//     &--info {
-//       color: $info
-//     }
+    &--info {
+      color: $info
+    }
 
-//     &--error {
-//       color: $error
-//     }
+    &--error {
+      color: $error
+    }
 
-//     &--success {
-//       color: $success
-//     }
-//   }
+    &--success {
+      color: $success
+    }
+  }
 
-//   &__container {
-//     width: 100%;
-//     text-align: left;
-//     background-color: rgba($help, .1);
-//     box-shadow: 0 0 0 2px $help-shadow;
-//     color: rgba($black, .7);
-//     border-radius: .25rem;
-//     padding: .5rem;
+  &__container {
+    width: 100%;
+    text-align: left;
+    background-color: rgba($help, .1);
+    box-shadow: 0 0 0 2px $help-shadow;
+    color: rgba($black, .7);
+    border-radius: .25rem;
+    padding: .5rem;
 
-//     &__title {
-//       margin-bottom: .25rem;
-//       font-size: $mobile-subheading;
-//       font-weight: bold;
-//     }
+    &__title {
+      margin-bottom: .25rem;
+      font-size: $mobile-subheading;
+      font-weight: bold;
+    }
 
-//     &--info {
-//       background-color: rgba($info, .15);
-//       box-shadow: 0 0 0 2px $info-shadow;
-//       color: $info
-//     }
+    &--info {
+      background-color: rgba($info, .15);
+      box-shadow: 0 0 0 2px $info-shadow;
+      color: $info
+    }
 
-//     &--error {
-//       background-color: rgba($error, .15);
-//       box-shadow: 0 0 0 2px $error-shadow;
-//       color: $error
-//     }
+    &--error {
+      background-color: rgba($error, .15);
+      box-shadow: 0 0 0 2px $error-shadow;
+      color: $error
+    }
 
-//     &--success {
-//       background-color: rgba($success, .15);
-//       box-shadow: 0 0 0 2px $success-shadow;
-//       color: $success
-//     }
-//   }
+    &--success {
+      background-color: rgba($success, .15);
+      box-shadow: 0 0 0 2px $success-shadow;
+      color: $success
+    }
+  }
 
-//   // &--highlight {
-//   //     font-weight: bold;
-//   // }
+  // &--highlight {
+  //     font-weight: bold;
+  // }
 
-//   &__link {
-//       font-weight: bold;
-//   }
-// }
+  &__link {
+      font-weight: bold;
+  }
+}
 </style>
